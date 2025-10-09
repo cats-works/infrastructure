0fc1b26c178685e2801a171f51b482b3910ece20
cat <cat@originull.org>
2025-10-09 10:44:29 -0700

    add multi-tool helper util

---
+++ shelltool.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SN=$(basename "$0")
+
+case "$SN" in
+    edit-vault)
+	ansible-vault edit /etc/ansible/group_vars/all/vault
+        ;;
+    pxe-boot)
+	ansible-playbook /etc/ansible/pxe-boot.yml --limit $1
+        ;;
+    deploy-vm)
+	ansible-playbook /etc/ansible/deploy-vm.yml --limit $1
+        ;;
+    install-galaxy)
+	ansible-galaxy install -r /etc/ansible/requirements.yaml
+        ;;
+    *)
+        echo "Unknown tool: $SN"
+        exit 1
+        ;;
+esac
